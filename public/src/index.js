acf54c1e53a902ef60e5bafdf945ef698fbdb9d5
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-29 07:18:25 +0530

    Create or join room

---
+++ public/src/index.js
@@ -0,0 +1,30 @@
+let isInitiator
+
+window.room = prompt("Enter room name:")
+
+let socket = io.connect()
+
+if (room !== "") {
+  console.log('Message from client: Asking to join room ' + room)
+  socket.emit('create or join', room)
+}
+
+socket.on('created', (room, clientId) => {
+  isInitiator = true
+})
+
+socket.on('full', (room) => {
+  console.log('Message from client: Room ' + room + ' is full :^(')
+})
+
+socket.on('ipaddr', (ipaddr) => {
+  console.log('Message from client: Server IP address is ' + ipaddr)
+})
+
+socket.on('joined', (room, clientId) => {
+  isInitiator = false
+})
+
+socket.on('log', (array) => {
+  console.log.apply(console, array)
+})
