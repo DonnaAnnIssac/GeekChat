1b64ce76341419af8c4f4d9b15c986468255dc19
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-31 15:37:34 +0530

    Emit separate events for creation of room and joining existing room and to notify peers about new peer

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const os = require('os')
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-
+// const uuid = require('node-uuid')
 app.use(express.static('./node_modules/socket.io-client'))
 app.use(express.static('./public'))
 
@@ -12,37 +11,32 @@ server.listen(9999, () => {
 })
 
 let clientCounter = 0
+let rooms = {}
+// let userIds = {}
 io.on('connection', client => {
   console.log('Connected')
-  client.on('message', message => {
-    client.to('test').emit('message', message)
+  client.on('message', (message, room) => {
+    client.to(room).emit('message', message, client.id)
   })
-  client.on('create or join', (room) => {
+  client.on('create or join', room => {
     console.log('Received request to create or join room ' + room)
-    let numClients = ++clientCounter
-    console.log('Room ' + room + ' now has ' + numClients + ' client(s)')
-    if (numClients === 1) {
+    if (!rooms[room]) {
+      rooms[room] = [client]
       client.join(room)
-      console.log('Client ID ' + client.id + ' created room ' + room)
-      client.emit('created', room, client.id)
-    } else if (numClients === 2) {
-      console.log('Client ID ' + client.id + ' joined room ' + room)
-      io.sockets.in(room).emit('join', room)
+      client.emit('created', room)
+    }
+    else {
+      rooms[room].push(client)
       client.join(room)
-      client.emit('joined', room, client.id)
-      io.sockets.in(room).emit('ready')
-    } else { // max two clients
-      client.emit('full', room)
+      client.emit('joined', room)
+      client.to(room).emit('new peer', room, client.id)
     }
   })
-  client.on('ipaddr', () => {
-    let ifaces = os.networkInterfaces()
-    for (let dev in ifaces) {
-      ifaces[dev].forEach((details) => {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-          client.emit('ipaddr', details.address)
-        }
-      })
-    }
-  })
+  // client.on('disconnect', room => {
+  //   let index = rooms[room].indexOf(client)
+  //   if (index !== -1) {
+  //     client.to(room).emit('disconnected', client)
+  //     rooms[room].splice(index, 1)
+  //   }
+  // })
 })
