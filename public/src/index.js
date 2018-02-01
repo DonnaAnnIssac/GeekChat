8fd57723982224c40bfe8aab8ffae6bcb7651649
Donna Ann Issac <donna.ann1209@gmail.com>
2018-02-01 16:10:28 +0530

    Send candidate messages at end of discovery

--- public/src/index.js
+++ public/src/index.js
@@ -1,58 +1,56 @@
-let sendData = document.getElementById('msg')
-let sendBtn = document.getElementById('sendMsg')
-let incomingMsg = document.getElementById('incomingMsg')
+let socket = io.connect()
+let localVideo = document.querySelector('#localVideo')
+let remoteFeeds = document.querySelector('#remoteFeeds')
 let isChannelReady = false
 let isInitiator = false
 let isStarted = false
-let localStream
-let pc
-let remoteStream
-let sendChannel, receiveChannel
-sendBtn.disabled = true
-sendBtn.addEventListener('click', sendMsgOverChannel)
+let localStream, remoteStream
+let pc, currentClient, candidates = [], pendingClients = [], myPeers = []
+let constraints = {
+  audio: true,
+  video: true
+}
 
 let room = prompt('Enter room name:')
 
-let socket = io.connect()
-
-if (room !== '') {
-  console.log('Message from client: Asking to join room ' + room)
+if (room || room === '') {
   socket.emit('create or join', room)
 }
 
-socket.on('created', (room, clientId) => {
+socket.on('created', room => {
+  isInitiator = true
+  getLocalStream()
+})
+
+socket.on('joined', room => {
+  isInitiator = false
+  // console.log('Just joined')
+  // console.log(myPeers.length)
+  // console.log(myPeers)
+  getLocalStream()
+})
+
+socket.on('new peer', room => {
+  isStarted = false
   isInitiator = true
 })
 
-socket.on('full', (room) => {
-  console.log('Message from client: Room ' + room + ' is full :^(')
-})
-
-socket.on('ipaddr', (ipaddr) => {
-  console.log('Message from client: Server IP address is ' + ipaddr)
-})
-
-socket.on('joined', (room, clientId) => {
-  isInitiator = false
-})
-
-socket.on('log', (array) => {
-  console.log(console, array)
-})
-
-function sendMessage (message) {
-  console.log('Client sending message: ', message)
-  socket.emit('message', message)
+function sendMessage (message, id) {
+  // console.log('Client sending message: ', message)
+  socket.emit('message', message, room, id)
 }
 
-// This client receives a message
-socket.on('message', (message) => {
-  console.log('Client received message:', message)
+socket.on('message', (message, id) => {
+  console.log('\n\nClient received message:', message)
+  if (myPeers.indexOf(id) === -1) currentClient = id
+  console.log('From: ', id)
+  console.log(myPeers.indexOf(id))
+  console.log('Current Peers: ')
+  console.log(myPeers)
   if (message === 'got user media') {
-    console.log('got user media')
     isChannelReady = true
     maybeStart()
-  } else if (message.type === 'offer') {
+  } else if (message.type === 'offer' && myPeers.indexOf(id) === -1) {
     if (!isInitiator && !isStarted) {
       isChannelReady = true
       maybeStart()
@@ -72,37 +70,29 @@ socket.on('message', (message) => {
   }
 })
 
-let localVideo = document.querySelector('#localVideo')
-let remoteVideo = document.querySelector('#remoteVideo')
-
-let constraints = {
-  audio: true,
-  video: true
+function getLocalStream () {
+  navigator.mediaDevices.getUserMedia(constraints)
+  .then(gotStream)
+  .catch((e) => {
+    alert('getUserMedia() error: ' + e.name)
+  })
 }
-navigator.mediaDevices.getUserMedia(constraints)
-.then(gotStream)
-.catch((e) => {
-  alert('getUserMedia() error: ' + e.name)
-})
 
 function gotStream (stream) {
-  console.log('Adding local stream.')
+  // console.log('Adding local stream.')
   localStream = stream
   localVideo.srcObject = stream
   sendMessage('got user media')
-  if (isInitiator) {
-    maybeStart()
-  }
 }
 
-function maybeStart () {
-  console.log('>>>>>>> maybeStart() ', isStarted, localStream, isChannelReady)
+function  maybeStart () {
+  // console.log('>>>>>>> maybeStart() ', isStarted, localStream, isChannelReady)
   if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
-    console.log('>>>>>> creating peer connection')
+    // console.log('>>>>>> creating peer connection')
     createPeerConnection()
     pc.addStream(localStream)
     isStarted = true
-    console.log('isInitiator', isInitiator)
+    // console.log('isInitiator', isInitiator)
     if (isInitiator) {
       doCall()
     }
@@ -116,15 +106,11 @@ window.onbeforeunload = function () {
 function createPeerConnection () {
   try {
     pc = new RTCPeerConnection(null)
-    sendChannel = pc.createDataChannel('sendDataChannel', null)
+    pendingClients.push(currentClient)
     pc.onicecandidate = handleIceCandidate
     pc.onaddstream = handleRemoteStreamAdded
     pc.onremovestream = handleRemoteStreamRemoved
-    sendChannel.onopen = handleSendChannelStateChange
-    sendChannel.onclose = handleSendChannelStateChange
-    pc.ondatachannel = receiveChannelCallback
-    sendBtn.disabled = false
-    console.log('Created RTCPeerConnnection')
+    // console.log('Created RTCPeerConnnection')
   } catch (e) {
     console.log('Failed to create PeerConnection, exception: ' + e.message)
     alert('Cannot create RTCPeerConnection object.')
@@ -132,54 +118,75 @@ function createPeerConnection () {
 }
 
 function handleIceCandidate (event) {
-  console.log('icecandidate event: ', event)
+  // console.log('icecandidate event: ', event.candidate)
+  // console.log('icecandidate target: ', event.target)
+  // console.log('current client: ', currentClient)
   if (event.candidate) {
-    sendMessage({
+    candidates.push({
       type: 'candidate',
       label: event.candidate.sdpMLineIndex,
       id: event.candidate.sdpMid,
       candidate: event.candidate.candidate
     })
   } else {
-    console.log('End of candidates.')
+    console.log('~~~~~~~~~~~~~~~~~~End of candidates.~~~~~~~~~~~~~~~~~~~~~~~~~')
+    sendCandidates()
   }
+}
+
+function sendCandidates () {
+  let client = pendingClients.shift()
+  candidates.forEach(candidate => {
+    console.log('Sending candidates to: ', client)
+    sendMessage(candidate, client)
+  })
+  candidates = []
+}
+
+function handleRemoteStreamAdded (event) {
+  // console.log('Remote stream added.')
+  remoteStream = event.stream
+  let remoteVideo = document.createElement('video')
+  remoteVideo.setAttribute('autoplay', true)
+  remoteVideo.srcObject = remoteStream
+  remoteFeeds.appendChild(remoteVideo)
+}
+
+function handleRemoteStreamRemoved (event) {
+  console.log('Remote stream removed. Event: ', event)
+}
+
+function doCall () {
+  // console.log('Sending offer to peer')
+  pc.createOffer()
+  .then(setLocalAndSendMessage)
+  .catch(handleCreateOfferError)
+}
+
+function doAnswer () {
+  // console.log('Sending answer to peer.')
+  pc.createAnswer()
+  .then(setLocalAndSendMessage)
+  .catch(onCreateSessionDescriptionError)
+  // console.log('After answering')
+  // console.log(myPeers)
+  // myPeers.push(currentClient)
+  // console.log(myPeers)
+}
+
+function setLocalAndSendMessage (sessionDescription) {
+  pc.setLocalDescription(sessionDescription)
+  myPeers.push(currentClient)
+  // console.log('setLocalAndSendMessage sending message', sessionDescription)
+  sendMessage(sessionDescription, currentClient)
 }
 
 function handleCreateOfferError (event) {
   console.log('createOffer() error: ', event)
 }
 
-function doCall () {
-  console.log('Sending offer to peer')
-  pc.createOffer(setLocalAndSendMessage, handleCreateOfferError)
-}
-
-function doAnswer () {
-  console.log('Sending answer to peer.')
-  pc.createAnswer().then(
-    setLocalAndSendMessage,
-    onCreateSessionDescriptionError
-  )
-}
-
-function setLocalAndSendMessage (sessionDescription) {
-  pc.setLocalDescription(sessionDescription)
-  console.log('setLocalAndSendMessage sending message', sessionDescription)
-  sendMessage(sessionDescription)
-}
-
 function onCreateSessionDescriptionError (error) {
   console.log('Failed to create session description: ' + error.toString())
-}
-
-function handleRemoteStreamAdded (event) {
-  console.log('Remote stream added.')
-  remoteStream = event.stream
-  remoteVideo.srcObject = remoteStream
-}
-
-function handleRemoteStreamRemoved (event) {
-  console.log('Remote stream removed. Event: ', event)
 }
 
 function handleRemoteHangup () {
@@ -192,42 +199,4 @@ function stop () {
   isStarted = false
   pc.close()
   pc = null
-}
-
-function sendMsgOverChannel (event) {
-  event.preventDefault()
-  let data = sendData.value
-  let msg = document.createElement('div')
-  msg.innerText = data
-  incomingMsg.appendChild(msg)
-  sendChannel.send(data)
-}
-
-function handleSendChannelStateChange () {
-  if (sendChannel.readyState === 'open') {
-    sendData.disabled = false
-    sendData.focus()
-    sendBtn.disabled = false
-  } else {
-    sendData.disabled = true
-    sendBtn.disabled = true
-  }
-}
-
-function receiveChannelCallback (event) {
-  receiveChannel = event.channel
-  receiveChannel.onmessage = onReceiveCallback
-  receiveChannel.onopen = handleReceiveChannelStateChange
-  receiveChannel.onclose = handleReceiveChannelStateChange
-}
-
-function onReceiveCallback (event) {
-  let msg = document.createElement('div')
-  msg.innerText = event.data
-  console.log('Appending')
-  incomingMsg.appendChild(msg)
-}
-
-function handleReceiveChannelStateChange () {
-  console.log(receiveChannel.readyState)
 }
