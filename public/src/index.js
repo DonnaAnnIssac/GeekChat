0348596bb8f437b8b49500c74db33a665030f10e
Donna Ann Issac <donna.ann1209@gmail.com>
2018-02-03 16:23:23 +0530

    Add data channel to send and receive messages from peers and send notifications to other peers when a peer leaves the room

--- public/src/index.js
+++ public/src/index.js
@@ -1,4 +1,8 @@
 let socket = io.connect()
+let sendData = document.getElementById('msg')
+let sendBtn = document.getElementById('sendMsg')
+let incomingMsg = document.getElementById('incomingMsg')
+let callBtn = document.getElementById('callButton')
 let localVideo = document.querySelector('#localVideo')
 let remoteFeeds = document.querySelector('#remoteFeeds')
 let isChannelReady = false
@@ -9,11 +13,18 @@ let constraints = {
   audio: true,
   video: true
 }
+sendBtn.disabled = true
+callBtn.disabled = true
+sendBtn.addEventListener('click', sendMsgOverChannel)
 
 let room = prompt('Enter room name:')
 
-if (room || room === '') {
-  socket.emit('create or join', room)
+if (room) {
+  callBtn.disabled = false
+  callBtn.addEventListener('click', () => {
+    callBtn.disabled = true
+    socket.emit('create or join', room)
+  })
 }
 
 socket.on('created', room => {
@@ -37,6 +48,7 @@ function sendMessage (message, id) {
 socket.on('message', (message, id) => {
   if (message === 'got user media') {
     isChannelReady = true
+    sendNotifications(id, 'joined')
     maybeStart(id)
   } else if (message.type === 'offer' && myPeers.indexOf(id) === -1) {
     if (!isInitiator) {
@@ -67,7 +79,7 @@ function getLocalStream () {
 function gotStream (stream) {
   localStream = stream
   localVideo.srcObject = stream
-  sendMessage('got user media')
+  // sendMessage('got user media')
 }
 
 function  maybeStart (id, message) {
@@ -89,13 +101,25 @@ window.onbeforeunload = function () {
 function createPeerConnection (id) {
   try {
     pcDictionary[id] = new RTCPeerConnection(null)
-    pcDictionary[id].clientId = id
     pcDictionary[id].onicecandidate = event => {
       handleIceCandidate(event, id)
     }
-    pcDictionary[id].onaddstream = handleRemoteStreamAdded
+    pcDictionary[id].onaddstream = event => {
+      handleRemoteStreamAdded(event, id)
+    }
     pcDictionary[id].onremovestream = handleRemoteStreamRemoved
     pcDictionary[id].addStream(localStream)
+    pcDictionary[id].sendChannel = pcDictionary[id].createDataChannel(room, null)
+    pcDictionary[id].sendChannel.onopen = () => {
+      handleSendChannelStateChange(pcDictionary[id].sendChannel)
+    }
+    pcDictionary[id].sendChannel.onclose = () => {
+      handleSendChannelStateChange(pcDictionary[id].sendChannel)
+    }
+    pcDictionary[id].ondatachannel = event => {
+      receiveChannelCallback(event, id)
+    }
+    sendBtn.disabled = false
   } catch (e) {
     console.log('Failed to create PeerConnection, exception: ' + e.message)
     alert('Cannot create RTCPeerConnection object.')
@@ -123,15 +147,58 @@ function sendCandidates (client) {
   candidates = []
 }
 
-function handleRemoteStreamAdded (event) {
+function handleRemoteStreamAdded (event, id) {
   let remoteVideo = document.createElement('video')
   remoteVideo.setAttribute('autoplay', true)
+  remoteVideo.setAttribute('id', id)
   remoteVideo.srcObject = event.stream
   remoteFeeds.appendChild(remoteVideo)
 }
 
 function handleRemoteStreamRemoved (event) {
   console.log('Remote stream removed. Event: ', event)
+}
+
+function handleSendChannelStateChange (sendChannel) {
+  if (sendChannel.readyState === 'open') {
+    sendData.disabled = false
+    sendData.focus()
+    sendBtn.disabled = false
+  } else {
+    sendData.disabled = true
+    sendBtn.disabled = true
+  }
+}
+
+function receiveChannelCallback (event, id) {
+  pcDictionary[id].receiveChannel = event.channel
+  pcDictionary[id].receiveChannel.onmessage = onReceiveCallback
+  pcDictionary[id].receiveChannel.onopen = () => {
+    handleReceiveChannelStateChange(id)
+  }
+  pcDictionary[id].receiveChannel.onclose = () => {
+    handleReceiveChannelStateChange(id)
+  }
+}
+
+function onReceiveCallback (event) {
+  let msg = document.createElement('div')
+  msg.innerText = event.data
+  incomingMsg.appendChild(msg)
+}
+
+function handleReceiveChannelStateChange (id) {
+  console.log(pcDictionary[id].receiveChannel.readyState)
+}
+
+function sendMsgOverChannel () {
+  let data = sendData.value
+  let msg = document.createElement('div')
+  msg.innerText = event.data
+  incomingMsg.appendChild(msg)
+  for (key in pcDictionary) {
+    pcDictionary[key].sendChannel.send(data)
+  }
 }
 
 function doCall (id) {
@@ -166,7 +233,13 @@ function handleRemoteHangup (id) {
   isInitiator = false
 }
 
-function stop () {
+function stop (id) {
   pcDictionary[id].close()
-  pcDictionary[id] = null
+  let disconnectedPeer = document.getElementById(id)
+  disconnectedPeer.parentNode.removeChild(disconnectedPeer)
+  sendNotifications(id, 'left')
+}
+
+function sendNotifications (id, action) {
+  document.getElementById('notifications').innerText = 'Client ' + id + ' ' + action
 }
