c6054a4cc63167eb88f2575a30237b8772ae8634
Donna Ann Issac <donna.ann1209@gmail.com>
2018-02-08 07:46:48 +0530

    Add separate events to handle group chats and dms

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const uuid = require('uuid/v4')
+const passport = require('passport')
+const Auth0Strategy = require('passport-auth0')
 app.use(express.static('./node_modules/socket.io-client'))
 app.use(express.static('./public'))
+
+const strategy = new Auth0Strategy(
+  {
+    domain: 'videochat-example.auth0.com',
+    clientID: 'qMllgLn5xiIpcpgsAMOF6sTmamaklhr8',
+    clientSecret: 'AsbvIRZlPGW7lTi1CJFSwCzwkFWSmEI6ulMJ3tz4NFEY3u17fH7nIddlO1ypqwGw',
+    callbackURL: 'http://localhost:3000/home'
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    return done(null, profile)
+  }
+)
+
+passport.use(strategy)
 
 server.listen(9999, () => {
   console.log('Listening on 9999')
@@ -36,9 +52,6 @@ io.on('connection', client => {
     client.join(room)
     client.emit('created', room, id)
   })
-  client.on('chat', (msg, id, room) => {
-    io.sockets.connected[id].emit('chat text', room, msg, client.id)
-  })
   client.on('init', (room, id) => {
     io.sockets.connected[id].emit('init', room)
   })
@@ -48,11 +61,19 @@ io.on('connection', client => {
     client.emit('joined', rooms[room])
     client.to(room).emit('accepted', rooms[room])
   })
-  client.on('disconnect', id => {
-    let index = rooms[room].indexOf(client)
-    if (index !== -1) {
-      client.to(room).emit('disconnected', client)
-      rooms[room].splice(index, 1)
-    }
+  client.on('chat', (msg, id, room) => {
+    io.sockets.connected[id].emit('chat text', msg, client.id)
   })
+  client.on('group chat', (msg, ids, room) => {
+    ids.forEach(id => {
+      io.sockets.connected[id].emit('group chat text', rooms[room], msg, client.id)
+    })
+  })
+  // client.on('disconnect', id => {
+  //   let index = rooms[room].indexOf(client)
+  //   if (index !== -1) {
+  //     client.to(room).emit('disconnected', client)
+  //     rooms[room].splice(index, 1)
+  //   }
+  // })
 })
