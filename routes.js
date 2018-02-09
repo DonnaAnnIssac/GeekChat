62afd35e641fc0ee8d45b01c0f3d8f7b2bda8e54
Donna Ann Issac <donna.ann1209@gmail.com>
2018-02-09 13:09:48 +0530

    Add redirectUrl, scope and /home handler

--- routes.js
+++ routes.js
@@ -6,23 +6,29 @@ const passport = require('passport')
 routes.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/login.html'))
 })
+
 routes.get('/login', passport.authenticate('auth0', {
   clientID: process.env.AUTH0_CLIENT_ID,
   domain: process.env.AUTH0_DOMAIN,
-  redirectUri: 'http://localhost:9999/home',
+  redirectUri: process.env.AUTH0_CALLBACK_URL,
   responseType: 'code',
-  audience: 'https://' + process.env.AUTH0_DOMAIN + '/',
-  scope: 'openid profile'}),
+  scope: 'openid profile'
+  }),
   (req, res) => {
     res.redirect('/home')
   }
 )
-routes.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+
+routes.get('/home', passport.authenticate('auth0', {failureRedirect: '/'}),
+  (req, res) => {
+    if (!req.user) {
+      throw new Error('user null')
+    }
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 routes.get('/logout', (req, res) => {
   res.redirect('/')
 })
-// add logout route
+
 module.exports = routes
